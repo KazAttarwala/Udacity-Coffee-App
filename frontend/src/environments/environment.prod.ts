581a5d1fc2b11d26879c65d9e28d50f93ab9021e
Qais Attarwala <qais.attarwala0@yahoo.com>
2021-10-31 17:33:42 -0500

    Update environment.prod.ts
    Added flask api url for Heroku production app.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiServerUrl: 'https://auth0-coffee-app.herokuapp.com/', // the running FLASK api server url
+  auth0: {
+    url: 'udacity-coffee-full-stack.us', // the auth0 domain prefix
+    audience: 'https://localhost5000', // the audience set for the auth0 app
+    clientId: '7SPGYBVrDhypVAaD5GebWZTeoSKuEL80', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
 };
